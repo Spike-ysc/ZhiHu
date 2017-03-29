474f5615ef04ce6cb75c48f0421f7a6ba2214af8
yan <shitakegongzhu@163.com>
2017-03-29 22:49:12 +0800

    顶部菜单大致完成，但是切换fragment还是卡

--- app/src/main/java/com/example/yan/zhihu/Item3Fragment.java
+++ app/src/main/java/com/example/yan/zhihu/Item3Fragment.java
@@ -2,6 +2,7 @@ package com.example.yan.zhihu;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +13,7 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class Item3Fragment extends Fragment {
-
+    private TabLayout tabLayout;
 
     public Item3Fragment() {
         // Required empty public constructor
@@ -22,8 +23,13 @@ public class Item3Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_item3,container,false);
+        tabLayout = (TabLayout) view.findViewById(R.id.tablayout3);
+        tabLayout.addTab(tabLayout.newTab().setText("tab1"));
+        tabLayout.addTab(tabLayout.newTab().setText("tab2"));
+        tabLayout.addTab(tabLayout.newTab().setText("tab3"));
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_item3, container, false);
+        return view;
     }
 
 }
--- app/src/main/java/com/example/yan/zhihu/MainActivity.java
+++ app/src/main/java/com/example/yan/zhihu/MainActivity.java
@@ -7,7 +7,9 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
+import android.view.Window;
 import android.widget.ListView;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
@@ -17,12 +19,19 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationB
 
     private BottomNavigationBar bar;
     private FloatingActionButton floating;
-
+    private Toolbar toolbar;
+    private Fragment currentFragment = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //悬浮按钮
         floating = (FloatingActionButton) findViewById(R.id.floating);
+        //顶部导航
+        toolbar = (Toolbar) findViewById(R.id.myToolBar);
+        toolbar.inflateMenu(R.menu.menu_test);
+
+        //底部菜单
         bar = (BottomNavigationBar) findViewById(R.id.main_bar);
         bar.setMode(BottomNavigationBar.MODE_FIXED)
                 .setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_DEFAULT);
@@ -43,25 +52,26 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationB
         switch (position){
             case 0:
                 Item1Fragment fragment1 = new Item1Fragment();
-
-                getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, fragment1).commit();
+                switchFragment(fragment1);
+//
+//                getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, fragment1).commit();
                 //Item1Fragment fragment1 = new Item1Fragment();
                 //getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, fragment1).commit();
                 break;
             case 1:
                 Item2Fragment fragment2 = new Item2Fragment();
-                getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment,fragment2).commit();
+               switchFragment(fragment2);
 
                // FourthFragment fragment4 = new FourthFragment();
                // getSupportFragmentManager().beginTransaction().replace(R.id.allFrame, fragment4).commit();
                 break;
             case 2:
-                Item1Fragment fragment3 = new Item1Fragment();
-                getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, fragment3).commit();
+                Item3Fragment fragment3 = new Item3Fragment();
+                switchFragment(fragment3);
                 break;
             case 3:
                 Item4Fragment fragment4 = new Item4Fragment();
-                getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, fragment4).commit();
+                switchFragment(fragment4);
             default:
                 break;
         }
@@ -75,5 +85,47 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationB
     public void onTabReselected(int position) {
 
     }
+    private void switchFragment(Fragment targetfragment){
+        FragmentTransaction transaction = getSupportFragmentManager()
+                .beginTransaction();
+        if(!targetfragment.isAdded()){
+            if(currentFragment != null){
+                transaction.hide(currentFragment);
+            }
+            transaction.add(R.id.main_fragment, targetfragment)
+                    .commit();
+        }else {
+            transaction.hide(currentFragment)
+                    .show(targetfragment)
+                    .commit();
+        }
+        currentFragment = targetfragment;
+    }
+
+//    private void switchFragment(Fragment from, Fragment to){
+//        if(from == null || to ==null){
+//
+//        }
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
